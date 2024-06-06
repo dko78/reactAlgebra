766d96a406f7b0af6d06fb8cb21ecf2086562923
Dalibor <kovacic.dalibor@gmail.com>
2024-06-06 14:54:57 +0200

    8

--- vjezba08_2/src/App.jsx
+++ vjezba08_2/src/App.jsx
@@ -5,17 +5,18 @@ import NotFound from "./pages/NotFound";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
-  return;
-  <Router>
-    <Header />
-    <main className="flex flex-col justify-between h-96">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/*" element={<NotFound />} />
-      </Routes>
-    </main>
-  </Router>;
+  return (
+    <Router>
+      <Header />
+      <main className="flex flex-col justify-between h-96">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+      </main>
+    </Router>
+  );
 };
 
 export default App;
--- vjezba08_2/src/components/IspisUsera.jsx
+++ vjezba08_2/src/components/IspisUsera.jsx
@@ -2,10 +2,15 @@ import { useEffect } from "react";
 
 const IspisUsera = () => {
   const fetchUsers = async () => {
-    const res = await fetch("http://api.github.com/users");
+    const res = await fetch("https://api.github.com/users");
     const data = await res.json();
     console.log(data);
   };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
   return <div>IspisUsera</div>;
 };
 
